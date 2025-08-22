96da43a211587e8e58efa824cc434dc6e23578df
unknown <bhaktiagrawal397@gmail.com>
2025-08-22 15:21:59 +0530

    Resolve conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
 import { SessionProvider } from "next-auth/react"
 import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
-import Loader from "./components/loader/Loader"
+import Loader from "./components/Loader/Loader"
 
 const poppins = Poppins({
   subsets: ["latin"],
